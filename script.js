401529b47171a60799b51d4528b836cda34467b9
Jacob <jake_chambers12@hotmail.com>
2020-02-10 10:12:11 -0500

    better: embedded editable google sheet into page

--- script.js
+++ script.js
@@ -1,80 +1,80 @@
 
 
 
-function submit () {
-  var method = document.getElementById('method').value
-  var startRow = hot.getSelected()[0][0]
-  var startCol = hot.getSelected()[0][1]
-  var endRow = hot.getSelected()[0][2]
-  var endCol = hot.getSelected()[0][3]
-  var sum = 0;
+// function submit () {
+//   var method = document.getElementById('method').value
+//   var startRow = hot.getSelected()[0][0]
+//   var startCol = hot.getSelected()[0][1]
+//   var endRow = hot.getSelected()[0][2]
+//   var endCol = hot.getSelected()[0][3]
+//   var sum = 0;
 
 
-  if (method == 'add'){
-    for(var i = startRow; i <= endRow; i++){
-      for (var j = startCol; j <= endCol; j++){
-        sum =  sum + parseInt(hot.getDataAtCell(i,j))
-      }
-    }
-  }
-  else if (method == 'subtract'){
-    for(var i = startRow; i <= endRow; i++){
-      for (var j = startCol; j <= endCol; j++){
-        sum =  sum - parseInt(hot.getDataAtCell(i,j))
-      }
-    }
-  }
-  else if (method == 'multiply'){
-    for(var i = startRow; i <= endRow; i++){
-      for (var j = startCol; j <= endCol; j++){
-        sum =  sum * parseInt(hot.getDataAtCell(i,j))
-      }
-    }
-  }
-  else if (method == 'divide'){
-    for(var i = startRow; i <= endRow; i++){
-      for (var j = startCol; j <= endCol; j++){
-        sum =  sum / parseInt(hot.getDataAtCell(i,j))
-      }
-    }
-  }
+//   if (method == 'add'){
+//     for(var i = startRow; i <= endRow; i++){
+//       for (var j = startCol; j <= endCol; j++){
+//         sum =  sum + parseInt(hot.getDataAtCell(i,j))
+//       }
+//     }
+//   }
+//   else if (method == 'subtract'){
+//     for(var i = startRow; i <= endRow; i++){
+//       for (var j = startCol; j <= endCol; j++){
+//         sum =  sum - parseInt(hot.getDataAtCell(i,j))
+//       }
+//     }
+//   }
+//   else if (method == 'multiply'){
+//     for(var i = startRow; i <= endRow; i++){
+//       for (var j = startCol; j <= endCol; j++){
+//         sum =  sum * parseInt(hot.getDataAtCell(i,j))
+//       }
+//     }
+//   }
+//   else if (method == 'divide'){
+//     for(var i = startRow; i <= endRow; i++){
+//       for (var j = startCol; j <= endCol; j++){
+//         sum =  sum / parseInt(hot.getDataAtCell(i,j))
+//       }
+//     }
+//   }
 
-  var cellToUpdate = document.getElementById('cellToUpdate').value;
-  var coordinates = cellToUpdate.split(',')
-  var row = parseInt(coordinates[0])
-  var column = parseInt(coordinates[1])
-  hot.setCellMeta(row, column, 'type', 'numeric');
-  hot.setDataAtCell(row, column, parseInt(sum))
-}
+//   var cellToUpdate = document.getElementById('cellToUpdate').value;
+//   var coordinates = cellToUpdate.split(',')
+//   var row = parseInt(coordinates[0])
+//   var column = parseInt(coordinates[1])
+//   hot.setCellMeta(row, column, 'type', 'numeric');
+//   hot.setDataAtCell(row, column, parseInt(sum))
+// }
 
 
 
-var container = document.getElementById('example');
-var data = [
-  [10, 11, 12, 13, 15, 16],
-  [10, 11, 12, 13, 15, 16],
-  [10, 11, 12, 13, 15, 16],
-  [10, 11, 12, 13, 15, 16],
-  [10, 11, 12, 13, 15, 16]
-]
-var hot = new Handsontable(container, {
-    rowHeaders: true,
-    colHeaders: true,
-    licenseKey: 'non-commercial-and-evaluation',
-    colWidths: 100,
-    dropdownMenu: true,
-    rowHeights: 25,
-    width: '100%',
-    height: 1000,
-    minSpareCols: 25,
-    minSpareRows: 50,
-    manualColumnResize: true,
-    manualRowResize:true,
-    columnSorting: true,
-    mergeCells: true,
-    outsideClickDeselects: false,
-    data:data,
-});
+// var container = document.getElementById('example');
+// var data = [
+//   [10, 11, 12, 13, 15, 16],
+//   [10, 11, 12, 13, 15, 16],
+//   [10, 11, 12, 13, 15, 16],
+//   [10, 11, 12, 13, 15, 16],
+//   [10, 11, 12, 13, 15, 16]
+// ]
+// var hot = new Handsontable(container, {
+//     rowHeaders: true,
+//     colHeaders: true,
+//     licenseKey: 'non-commercial-and-evaluation',
+//     colWidths: 100,
+//     dropdownMenu: true,
+//     rowHeights: 25,
+//     width: '100%',
+//     height: 1000,
+//     minSpareCols: 25,
+//     minSpareRows: 50,
+//     manualColumnResize: true,
+//     manualRowResize:true,
+//     columnSorting: true,
+//     mergeCells: true,
+//     outsideClickDeselects: false,
+//     data:data,
+// });
 
 
 // Change cell
